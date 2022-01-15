9dd11d601bd5ce91199ba4d788cd5f615f97b926
dev-traveler <avenj.dev@gmail.com>
2022-01-15 23:15:56 +0900

    Add Stack component

---
+++ src/components/element/Stack/Stack.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MuiStack, { StackProps } from "@mui/material/Stack";
+
+function Stack(props: StackProps) {
+  return <MuiStack {...props} />;
+}
+
+export default Stack;

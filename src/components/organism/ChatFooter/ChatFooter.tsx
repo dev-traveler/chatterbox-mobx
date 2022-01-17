4e8683eaf613c705afd52e183c5fa91e162d05c7
dev-traveler <avenj.dev@gmail.com>
2022-01-18 00:16:19 +0900

    Make ChatFooter add message to messageStore

--- src/components/organism/ChatFooter/ChatFooter.tsx
+++ src/components/organism/ChatFooter/ChatFooter.tsx
@@ -1,10 +1,33 @@
-import React from "react";
-import Stack from "../../element/Stack";
+import React, { useState } from "react";
+import useStores from "hooks/useStores";
+import Stack from "components/element/Stack";
+import TextField from "components/element/TextField";
+import Message from "model/Message";
 
 function ChatFooter() {
+  const { messageStore } = useStores();
+  const [text, setText] = useState("");
+
+  const handleSubmitText = (event: any) => {
+    if (event.key === "Enter") {
+      messageStore.add(new Message({ text }));
+      setText("");
+    }
+  };
+
+  const handleTextChange = (event: any): void => {
+    setText(event.target.value);
+  };
+
   return (
     <Stack direction="row" sx={{ backgroundColor: "#ffd66c" }}>
-      ChatFooter
+      <TextField
+        sx={{ flex: 1 }}
+        placeholder="메세지를 입력하세요"
+        value={text}
+        onChange={handleTextChange}
+        onKeyDown={handleSubmitText}
+      />
     </Stack>
   );
 }

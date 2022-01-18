7d6dd6a6d9efb539f8458b3e191174b71b8fd423
dev-traveler <avenj.dev@gmail.com>
2022-01-18 23:20:49 +0900

    Add id and createdTs when Message is added to messageStore

--- src/components/organisms/ChatFooter/ChatFooter.tsx
+++ src/components/organisms/ChatFooter/ChatFooter.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import useStores from "hooks/useStores";
 import Stack from "components/elements/Stack";
 import TextField from "components/elements/TextField";
@@ -8,14 +9,20 @@ function ChatFooter() {
   const { messageStore } = useStores();
   const [text, setText] = useState("");
 
-  const handleSubmitText = (event: any) => {
+  const handleKeyDown = (event: any) => {
     if (event.key === "Enter") {
-      messageStore.add(new Message({ text }));
+      messageStore.add(
+        new Message({
+          id: uuidv4(),
+          text,
+          createdTs: new Date().getTime(),
+        })
+      );
       setText("");
     }
   };
 
-  const handleTextChange = (event: any): void => {
+  const handleChange = (event: any): void => {
     setText(event.target.value);
   };
 
@@ -25,8 +32,8 @@ function ChatFooter() {
         sx={{ flex: 1 }}
         placeholder="메세지를 입력하세요"
         value={text}
-        onChange={handleTextChange}
-        onKeyDown={handleSubmitText}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
       />
     </Stack>
   );

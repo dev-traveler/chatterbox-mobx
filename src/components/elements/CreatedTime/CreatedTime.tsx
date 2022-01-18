c8c9dcf662cd1b913c42b9bea44c0ef361e8bb63
dev-traveler <avenj.dev@gmail.com>
2022-01-18 23:47:34 +0900

    Fix marginBottom of createdTime

--- src/components/elements/CreatedTime/CreatedTime.tsx
+++ src/components/elements/CreatedTime/CreatedTime.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
 import Stack from "components/elements/Stack";
+import Box from "../Box";
 
 function CreatedTime(props: CreatedTimeProps): JSX.Element {
-  const createdTime = format(props.createdTs, 'aaa hh:mm', {locale: ko});
+  const createdTime = format(props.createdTs, "aaa hh:mm", { locale: ko });
 
-  return <Stack sx={{ justifyContent: 'flex-end', mb: 1 }}>{createdTime}</Stack>;
+  return (
+    <Stack sx={{ justifyContent: "flex-end" }}>
+      <Box sx={{  mb: '4px' }}>{createdTime}</Box>
+    </Stack>
+  );
 }
 
 interface CreatedTimeProps {

9a866ef9de6d789589faa7b0a72afad14029df6a
dev-traveler <avenj.dev@gmail.com>
2022-01-20 00:59:17 +0900

    Add Avatar component

---
+++ src/components/elements/Avatar/Avatar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MuiAvatar, { AvatarProps } from "@mui/material/Avatar";
+
+function Avatar(props: AvatarProps) {
+  return <MuiAvatar {...props} />;
+}
+
+export default Avatar;

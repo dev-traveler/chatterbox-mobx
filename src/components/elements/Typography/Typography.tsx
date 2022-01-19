205bf5839b16dbfce6a8c111e6a592e4a7187518
dev-traveler <avenj.dev@gmail.com>
2022-01-19 23:02:20 +0900

    Add Typography component

---
+++ src/components/elements/Typography/Typography.tsx
@@ -0,0 +1,8 @@
+import MuiTypography from "@mui/material/Typography";
+import { TypographyProps } from "@mui/material/Typography/Typography";
+
+function Typography(props: TypographyProps) {
+  return <MuiTypography {...props} />;
+}
+
+export default Typography;

a32d6fb708b444116bec0ad071b44adf6338bd62
dev-traveler <avenj.dev@gmail.com>
2022-01-21 23:17:27 +0900

    Implement MessageStack to show continuous messages

--- src/components/organisms/MessageStack/MessageStack.tsx
+++ src/components/organisms/MessageStack/MessageStack.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { computed } from "mobx";
+import { observer } from "mobx-react";
+import { format } from "date-fns";
+import { ko } from "date-fns/locale";
+import useStores from "hooks/useStores";
 import Stack from "components/elements/Stack";
 import Avatar from "components/elements/Avatar";
 import TextBubble from "components/bubbles/TextBubble";
@@ -6,15 +11,33 @@ import CreatedTime from "components/elements/CreatedTime";
 import { IMessage } from "model/Message";
 
 function MessageStack(props: MessageStackProps): JSX.Element {
-  const { text, createdTs } = props.message;
+  const { messageStore } = useStores();
+  const { id, text, createdTs } = props.message;
+
+  const prevMsg = computed(() => messageStore.getPrevMsg(id)).get();
+  const nextMsg = computed(() => messageStore.getNextMsg(id)).get();
+  const prevMsgCreatedTime = format(prevMsg?.createdTs || 0, "aaa hh:mm", {
+    locale: ko,
+  });
+  const nextMsgCreatedTime = format(nextMsg?.createdTs || 0, "aaa hh:mm", {
+    locale: ko,
+  });
+  const currMsgCreatedTime = format(createdTs, "aaa hh:mm", { locale: ko });
+  const isSameTimeWithPrev = prevMsgCreatedTime === currMsgCreatedTime;
+  const isSameTimeWithNext = nextMsgCreatedTime === currMsgCreatedTime;
 
   return (
-    <Stack direction="row" spacing={1} sx={{ mt: 1 }}>
-      <Stack sx={{ justifyContent: "center" }}>
+    <Stack direction="row" spacing={1} sx={{ mt: isSameTimeWithPrev ? '2px' : '10px' }}>
+      <Stack
+        sx={{
+          justifyContent: "center",
+          visibility: isSameTimeWithPrev ? "hidden" : "",
+        }}
+      >
         <Avatar />
       </Stack>
       <TextBubble text={text} />
-      <CreatedTime createdTs={createdTs} />
+      {!isSameTimeWithNext && <CreatedTime createdTs={createdTs} />}
     </Stack>
   );
 }
@@ -23,4 +46,4 @@ interface MessageStackProps {
   message: IMessage;
 }
 
-export default MessageStack;
+export default observer(MessageStack);
--- src/store/messageStore.ts
+++ src/store/messageStore.ts
@@ -1,11 +1,11 @@
 import { makeAutoObservable, observable } from "mobx";
 import { IRootStore } from "store/rootStore";
-import { IMessage } from "model/Message";
+import Message from "model/Message";
 
 class MessageStore implements IMessageStore {
   rootStore: IRootStore;
 
-  messages: IMessage[] = [];
+  messages: Message[] = [];
 
   constructor(rootStore: IRootStore) {
     this.rootStore = rootStore;
@@ -15,14 +15,33 @@ class MessageStore implements IMessageStore {
     });
   }
 
-  add(messages: IMessage): void {
+  add(messages: Message): void {
     this.messages.push(messages);
+  }
+
+  getMsgIndex(id: string): number {
+    return this.messages.findIndex((message) => message.id === id);
+  }
+
+  getPrevMsg(id: string): Message | undefined {
+    const currIndex = this.getMsgIndex(id);
+    if (currIndex === -1) return undefined;
+    return this.messages[currIndex - 1];
+  }
+
+  getNextMsg(id: string): Message | undefined {
+    const currIndex = this.getMsgIndex(id);
+    if (currIndex === -1) return undefined;
+    return this.messages[currIndex + 1];
   }
 }
 
 export interface IMessageStore {
-  messages: IMessage[];
-  add(messages: IMessage): void;
+  messages: Message[];
+  add(messages: Message): void;
+  getMsgIndex(id: string): number;
+  getPrevMsg(id: string): Message | undefined;
+  getNextMsg(id: string): Message | undefined;
 }
 
 export default MessageStore;
